b3967eaa3a0c48ba3267108d537cfc80ca251b1e
vlodzina-ship-it <vlodzina@gmail.com>
2026-06-10 13:45:39 +0200

    Update service worker cache version and files

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
-const CACHE_NAME = "protokol-dssp-v1-4";
+const CACHE_NAME = "protokol-dssp-v1-7";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./logo-upcr.png"
+  "./style.css?v=1.6",
+  "./app.js?v=1.6",
+  "./manifest.json?v=1.6",
+  "./logo-upcr.png?v=1.6"
 ];
 
 self.addEventListener("install", function(event){
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache){
       return cache.addAll(FILES_TO_CACHE);
@@ -27,14 +29,36 @@ self.addEventListener("activate", function(event){
           }
         })
       );
+    }).then(function(){
+      return self.clients.claim();
     })
   );
 });
 
 self.addEventListener("fetch", function(event){
+
+  if(event.request.mode === "navigate"){
+    event.respondWith(
+      fetch(event.request).catch(function(){
+        return caches.match("./index.html");
+      })
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(function(response){
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(function(response){
+        var responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then(function(cache){
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(function(){
+        return caches.match(event.request);
+      })
   );
 });
